11a7d448180e1d1ae1dcef8ac610b3a4caf9c6ba
yannic <yannic.ellhotka@gmail.com>
2023-08-01 11:02:05 +0200

    feat(docs): repalced docus docs with vitepress

---
+++ packages/docs/.vitepress/config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitepress';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+	title: '@vuetils/form',
+	description: 'Tiny form valdiation library for vue 3',
+	themeConfig: {
+		// https://vitepress.dev/reference/default-theme-config
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'Examples', link: '/markdown-examples' },
+		],
+
+		sidebar: [
+			{
+				text: 'Examples',
+				items: [
+					{ text: 'Markdown Examples', link: '/markdown-examples' },
+					{ text: 'Runtime API Examples', link: '/api-examples' },
+				],
+			},
+		],
+
+		socialLinks: [{ icon: 'github', link: 'https://github.com/YannicEl/vue-useForm' }],
+	},
+
+	sitemap: {
+		hostname: 'https://example.com',
+		lastmodDateOnly: true,
+	},
+});
