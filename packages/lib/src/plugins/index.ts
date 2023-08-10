09d954754484e11f8a99169153ffa1db6c0d9645
yannic <yannic.ellhotka@gmail.com>
2023-08-10 21:29:03 +0200

    fix(lib)!: made type arguments optional on definePlugin

--- packages/lib/src/plugins/index.ts
+++ packages/lib/src/plugins/index.ts
@@ -5,7 +5,7 @@ export * from './storagePlugin';
 export type Plugin<TForm extends Form = Form> = (form: TForm) => void;
 export type PluginFactory<TForm extends Form = Form> = (...params: any) => Plugin<TForm>;
 
-export function definePlugin<TArgs, TForm extends Form = Form>(
+export function definePlugin<TArgs = undefined, TForm extends Form = Form>(
 	fn: (form: TForm, args: TArgs) => void
 ): PluginFactory<TForm> {
 	return (args: TArgs) => (form: TForm) => {
