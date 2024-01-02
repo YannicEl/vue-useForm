5af10892a29acc108b0c32c03077d21a66cc28fc
Yannic <me@yannic.at>
2024-01-02 17:53:00 +0000

    feat: added UFormGroup component

---
+++ packages/lib/src/components/UFormGroup.vue
@@ -0,0 +1,14 @@
+<template>
+	<slot />
+</template>
+
+<script setup lang="ts">
+import { provideForm } from '../composables/useFormInject';
+import type { Form } from '../useForm';
+
+const { form } = defineProps<{
+	form: Form;
+}>();
+
+provideForm(form);
+</script>
--- packages/lib/src/components/index.ts
+++ packages/lib/src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as UField } from './UField.vue';
 export { default as UForm } from './UForm.vue';
+export { default as UFormGroup } from './UFormGroup.vue';
